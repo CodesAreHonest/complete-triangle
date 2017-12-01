ed88e82698d8ca1fc0a91a8981c0c5c8ef7942e8
Yinghua <yinghuachai0@gmail.com>
2017-12-01 16:18:17 +0800

    implements function to check whether stdin is numerical or string

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,41 @@ use std::io::Write;
 
 fn main() {
 
-    print!("Input variable a -> ");
+    let (a, is_num) = obtain_input("a");
+    println!("The variable A is {} and is a number ({})", a, is_num);
+
+}
+
+// the function check whether the input is number or string, then return number
+fn obtain_input(var: &str) -> (f64, bool) {
+
+    print!("Input number for variable {} -> ", var);
 
     // add flush() to prevent frequently line-buffered
     io::stdout().flush().unwrap();
-    let mut a = String::new();
-    io::stdin().read_line(&mut a).expect(
-        "Variable A failed to read from Stdin",
-    );
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("readline error");
 
-    let trimmed_a = a.trim();
-    match trimmed_a.parse::<u32>() {
-        Ok(integer) => println!("The first integer input is: {}", integer),
-        Err(..) => println!("The input '{}' is not an integer. ", trimmed_a),
+    let trimmed_input = input.trim();
+
+    // check if the input is a number or floats
+    let is_num = trimmed_input.parse::<f64>().is_ok();
+
+    // display the message to check whether the inputs are numerical
+    match trimmed_input.parse::<f64>() {
+        Ok(..) => println!("The input ({}) is a number. ", trimmed_input),
+        Err(..) => println!("The input ({}) is not a number. ", trimmed_input),
     };
+
+
+    if is_num == true {
+        let converted_value = trimmed_input.parse().unwrap();
+        return (converted_value, is_num);
+    } else {
+        let default_value = 0.0;
+        return (default_value, is_num);
+    }
+
+
+
 }
