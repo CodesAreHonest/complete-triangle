8752126c444f096edee903c487b0385c87ccefea
Yinghua <yinghuachai0@gmail.com>
2017-12-02 22:22:24 +0800

    fix decimal read error

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,22 @@ use std::io::Write;
 
 fn main() {
 
-    let (mut a, is_a_int) = obtain_input("a");
-    let (mut b, is_b_int) = obtain_input("b");
-    let (mut c, is_c_int) = obtain_input("c");
+    let mut a = obtain_input("a");
+    let mut b = obtain_input("b");
+    let mut c = obtain_input("c");
+
+    println!("a = {}, b = {}, c = {}", a, b, c);
 
 }
 
 // the function check whether the input is number or string, then return number
-fn obtain_input(var: &str) -> (u64, bool) {
+fn obtain_input(var: &str) -> u64 {
 
     loop {
-        print!("Input number for variable {} -> ", var);
+        print!(
+            "Input number for variable {} (between 1 to 1000000) -> ",
+            var
+        );
 
         // add flush() to prevent frequently line-buffered
         io::stdout().flush().unwrap();
@@ -26,19 +31,31 @@ fn obtain_input(var: &str) -> (u64, bool) {
         let is_num = trimmed_input.parse::<u64>().is_ok();
 
         // display the message to check whether the inputs are numerical
-        match trimmed_input.parse::<u64>() {
+        match trimmed_input.parse::<u64>() {	
             Ok(..) => println!("The input ({}) is a integer. ", trimmed_input),
             Err(..) => println!("The input ({}) is not a integer. ", trimmed_input),
         };
 
+        let input: u64;
 
         if is_num == true {
             let converted_value = trimmed_input.parse().unwrap();
-            return (converted_value, is_num);
+            input = converted_value;
+        } else {
+            input = 0;
         }
 
+        // check whether the number input are among 1 to 1000000
+        if input >= 1 && input <= 1000000 {
+            let valid_num = true;
+
+            if is_num == true && valid_num {
+                //            let converted_value = trimmed_input.parse().unwrap();
+                return input;
+            }
+        } else {
+            println!("Your input is not between 1 to 1000000, please type again");
+        }
 
     }
-
-
 }
