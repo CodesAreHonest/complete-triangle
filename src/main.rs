2aefc2a24a27744bfaf29e24b0b6ff159d269188
Yinghua <yinghuachai0@gmail.com>
2017-12-02 12:19:08 +0800

    only accept integer not decimals

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,15 @@ use std::io::Write;
 fn main() {
 
     let (a, is_num) = obtain_input("a");
-    println!("The variable A is {} and is a number ({})", a, is_num);
+    //    println!("The variable A is {} and is a number ({})", a, is_num);
+
+    let (b, is_num) = obtain_input("b");
+    let (c, is_num) = obtain_input("c");
 
 }
 
 // the function check whether the input is number or string, then return number
-fn obtain_input(var: &str) -> (f64, bool) {
+fn obtain_input(var: &str) -> (u64, bool) {
 
     print!("Input number for variable {} -> ", var);
 
@@ -21,23 +24,19 @@ fn obtain_input(var: &str) -> (f64, bool) {
     let trimmed_input = input.trim();
 
     // check if the input is a number or floats
-    let is_num = trimmed_input.parse::<f64>().is_ok();
+    let is_num = trimmed_input.parse::<u64>().is_ok();
 
     // display the message to check whether the inputs are numerical
-    match trimmed_input.parse::<f64>() {
-        Ok(..) => println!("The input ({}) is a number. ", trimmed_input),
-        Err(..) => println!("The input ({}) is not a number. ", trimmed_input),
+    match trimmed_input.parse::<u64>() {
+        Ok(..) => println!("The input ({}) is a integer. ", trimmed_input),
+        Err(..) => println!("The input ({}) is not a integer. ", trimmed_input),
     };
-
 
     if is_num == true {
         let converted_value = trimmed_input.parse().unwrap();
         return (converted_value, is_num);
     } else {
-        let default_value = 0.0;
+        let default_value = 0;
         return (default_value, is_num);
     }
-
-
-
 }
